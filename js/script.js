235d2f9858596f95bab358fc2c866e45605f6822
shainasuri <suris546@newschool.edu>
2021-05-03 16:49:44 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+new Glide('.glide', {
+    type: 'carousel', 
+    startAt: 0, 
+    perView: 1.2,
+    perTouch: 1,
+    gap: 100,
+    // autoplay: 2000,
+    // breakpoints: {
+    //     1400: {
+    //         perView: 5,
+    //     },
+    //     1024: {
+    //         perView: 3,
+    //         gap: 200,
+    //     },
+    //     600: {
+    //         perView: 1,
+    //     }
+    // }
+}).mount();
